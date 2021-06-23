87054d8f5ef7d4a59a4e208746523813446e9933
mueez-uddin <86313423+mueez-uddin@users.noreply.github.com>
2021-06-23 18:38:37 +0500

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,14 @@
+const plane = document.querySelectorAll('.plane')
+
+plane.forEach((plane)=>{
+    plane.addEventListener('click', () =>{
+        removeActiveClasses()
+        plane.classList.add("active")
+    })
+})
+
+function removeActiveClasses() {
+    plane.forEach(plane =>{
+        plane.classList.remove('active')
+    })
+} 
